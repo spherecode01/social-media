6039b6aef383b992447944114bc6b5b36b8f0a35
simran <simranag951@gmail.com>
2024-02-22 11:40:04 +0530

    added backend link

--- client/src/scenes/widgets/UserWidget.jsx
+++ client/src/scenes/widgets/UserWidget.jsx
@@ -17,7 +17,7 @@ import { ManageAccountsOutlined, EditOutlined,LocationOnOutlined, WorkOutlineOut
     const main = palette.neutral.main;
 
     const getUser = async () => {
-        const response = await fetch(`http://localhost:3001/users/${userId}`, {
+        const response = await fetch(`https://social-media-7j87.onrender.com/users/${userId}`, {
             method: "GET",
             headers: {Authorization: `Bearer ${token}`},
         });
